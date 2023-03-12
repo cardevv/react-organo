a585c502a54afe554a95e993240c9f499f4fc7d7
cardevv <113392365+cardevv@users.noreply.github.com>
2023-03-11 22:26:10 -0300

    eventos do botão

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -3,13 +3,21 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
+    let valor = ''
+
+    const aoDigitar = (evento) => {
+
+        valor = evento.target.value
+            console.log(valor)
+    }
+
     return (
 
         <div className="campo-texto">
             <label >
                {props.label}
             </label>
-            <input placeholder={props.placeholder} /> 
+            <input  value={valor}    onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} /> 
             
 
         </div>
--- organo/src/componentes/form/index.js
+++ organo/src/componentes/form/index.js
@@ -5,31 +5,43 @@ import Botao from '../botao'
 
 
 
-const times = [
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops',
-    'Ux e Design',
-    'Mobile',
-    'Inovação e Gestão'
-  
-  ]
+
 
 
 const Form = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+      
+      ]
+
+      const Salvaform = (evento) => {
+
+        evento.preventDefault()
+
+        
+        console.log('Formulario foi enviado!')
+
+
+      }
 
     return (
 
         <section className="formulario"> 
             
 
-            <form>
+            <form  onSubmit={Salvaform} >
             <h2>Preenhca os dados parar criar o card do colaborador</h2>
-            <CampoTexto  label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto  label="Cargo" placeholder="Digite seu cargo"/>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
             <CampoTexto  label="Imagem" placeholder="Insira sua imagem"/>
-            <ListaSuspensa label="Time" itens={times}/>
+            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
             <Botao>
                 Cria Card
             </Botao>
