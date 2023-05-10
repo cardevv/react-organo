d72da44ca4dda9a1099ea7e3e5de86bdb8fbaecc
cardevv <113392365+cardevv@users.noreply.github.com>
2023-05-10 00:07:21 -0300

    criando componente time e preparando para receber dados

--- organo/src/App.js
+++ organo/src/App.js
@@ -3,11 +3,54 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Form from './componentes/form';
+import Time from './componentes/time';
+
+
 
 
 
 
 function App() {
+
+  const times = [
+
+    {
+      nome:'Programação' ,
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome:        'Front-End' ,
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome:        'Data Science' ,
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome:        'Devops' ,
+      corPrimaria: '#e06869',
+      corSecundaria: '#fde7eb'
+    },
+    {
+      nome:         'Ux e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome:        'Mobile' ,
+      corPrimaria: '#ff8ao5',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome:        'Inovação e Gestão' ,
+      corPrimaria: '#ffba29',
+      corSecundaria: '#ffeedf'
+    }
+
+  ]
 
   const [colaboradores,setColaboradores] = useState([])
 
@@ -26,7 +69,8 @@ function App() {
       
       <Banner />
       <Form aoCadastrar={colaborador => aoNovoColaborador(colaborador)} />
-      
+
+       {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}></Time>)}
 
 
       
---
+++ organo/src/componentes/time/index.js
@@ -0,0 +1,19 @@
+import './time.css'
+
+
+const Time = (props) => { 
+
+    const css = {backgroundColor: props.corSecundaria}
+
+    return (
+        <section className='time'style={css} >
+            <h3 style={{borderColor: props.corPrimaria}}>
+                {props.nome}
+            </h3>
+        </section>
+
+    )
+
+}
+
+export default Time
